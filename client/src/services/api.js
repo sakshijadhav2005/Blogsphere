95b7c547aa0060d50af53297d3c602561749a2b3
sakshijadhav2005 <jadhavsakshi389@gmail.com>
2025-04-06 00:00:18 +0530

    final commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://blogspherebackendsakshi/api",
 });
 
 // Attach token to requests
