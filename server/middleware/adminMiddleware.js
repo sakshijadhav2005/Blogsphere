b72392aa30cd4b960476da3e006193dd14e3cc84
sakshijadhav2005 <jadhavsakshi389@gmail.com>
2025-04-06 00:42:24 +0530

    commit

--- server/middleware/adminMiddleware.js
+++ server/middleware/adminMiddleware.js
@@ -1,5 +1,5 @@
 const adminMiddleware = (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
+    if (req.user && req.user.isAdmin) {
       next();
     } else {
       res.status(403).json({ message: "Access Denied: Admins Only" });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
 
 const express = require("express");
 const { signup, login, forgotPassword, resetPassword,getAllUsers,deleteUser} = require("../Controller/authController");
-
+const adminMiddleware = require("../middleware/adminMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 
-router.get("/getallusers", getAllUsers); // Get all users
-router.delete("/deleteuser/:id", deleteUser); // Delete user by ID
+router.get("/getallusers",authMiddleware,adminMiddleware, getAllUsers); // Get all users
+router.delete("/deleteuser/:id",authMiddleware,adminMiddleware, deleteUser); // Delete user by ID
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
